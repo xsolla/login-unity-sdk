38f028eb289bb72a869abfd551beff7a0d30e7b8
a_kislaya <a.kislaya@vironit.co.uk>
2019-05-15 10:58:29 +0300

    Clickable documentation URL

---
+++ Assets/Xsolla/AuthDemo/Assets/Scripts/OpenDocumentation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OpenDocumentation : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.OpenURL("https://github.com/xsolla/login-unity-sdk/blob/master/README.md");
+    }
+}
